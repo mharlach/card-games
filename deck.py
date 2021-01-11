9e2046fe002da34f37813f69ffc3a3b1d87c7b58
Mike Harlach <michaelharlach@gmail.com>
2021-01-11 08:57:36 -0500

    Added items

---
+++ deck.py
@@ -0,0 +1,23 @@
+from random import randint
+from card import Card
+
+class Deck:
+    def __init__(self):
+        self.cards =[]
+        suits = ["Clubs","Diamonds","Spades","Hearts"]
+        for s in suits:
+            for v in range(1,14):
+                self.cards.append(Card(s,v))
+            
+
+    def shuffle(self):
+        for n in range(0,51):
+            a = randint(0,51)
+            b = randint(0,51)
+            cardA = self.cards[a]
+            cardB = self.cards[b]
+            self.cards[a]=cardB
+            self.cards[b]= cardA
+
+    def takeCard(self):
+        return self.cards.pop(0)
